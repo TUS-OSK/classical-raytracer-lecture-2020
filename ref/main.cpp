ee13e6aa9ce216ee32c606f8efa48698e3bfa3c1
yumcyawiz <hashdolf1923@gmail.com>
2021-01-24 11:42:07 +0900

    add image.h

--- ref/main.cpp
+++ ref/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
+#include "image.h"
 #include "ray.h"
 
 int main() {
-  Vec3f v1(1, 0, 0);
-  Vec3f v2(0, 1, 0);
-  std::cout << cross(v1, v2) << std::endl;
+  Image img(512, 512);
+
+  for (int j = 0; j < img.getWidth(); ++j) {
+    for (int i = 0; i < img.getHeight(); ++i) {
+      img.setPixel(i, j,
+                   Vec3f(static_cast<float>(i) / img.getWidth(),
+                         static_cast<float>(j) / img.getHeight(), 1.0f));
+    }
+  }
+  img.writePPM("output.ppm");
+
   return 0;
 }
